009b15b6dd6f6488d40597b8007ab4a1fff05dcc
unknown <gaurijkjay@gmail.com>
2026-01-15 12:51:18 +0530

    twice largest

--- src/LargestTwice.java
+++ src/LargestTwice.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 public class LargestTwice {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        
+        int n=scan.nextInt();
+        int[] arr =new int[n];
+        
+        for (int i=0;i<n;i++) {
+            arr[i]=scan.nextInt();
+        }
+        int index=-1;
+        int largest=arr[0];
+        for (int i=0;i<n;i++) {
+            if (arr[i]>largest) {
+                largest = arr[i];
+                index=i;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(i!=index && arr[i]*2>largest){
+                System.out.println("-1");
+                break;
+            } 
+        }
+        System.out.println(index); 
     
-        // TODO: Read n
-        // TODO: Read array elements
-        // TODO: Find the largest element and its index
-        // TODO: Check if largest >= 2 * every other element
-        // TODO: Print index or -1
-    
+    }
+
 }
